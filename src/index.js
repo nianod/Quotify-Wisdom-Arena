2893ae8571f02df530014a7ff642959349046d48
nianod <arnoldkk422@gmail.com>
2025-03-23 13:13:13 +0300

    dd commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+let quotes = [
+    "Sometimes we will never know the true value of a moment untill it becomes a memory",
+    "Follow your curiosity",
+    "A goal without a plan is just a wish",
+    "Stay humble, Good times will coome"
+]
+const noRpeat = new Set();
 const showQuote = document.getElementById("getQuote");
 const generate = document.getElementById("submitBtn");
 
 
 function generateQuote() {
-    showQuote.innerHTML = "Never give up";
+    let randomQuote = Math.floor(Math.random() * quotes.length);
+    let quote = quotes[randomQuote]
+    showQuote.innerHTML = quote;
 }
 generate.onclick = generateQuote;
