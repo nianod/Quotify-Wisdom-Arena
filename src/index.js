eaf744c0acbe4a1f51a19dcb8b98985a58d086c7
nianod <arnoldkk422@gmail.com>
2025-03-23 13:23:19 +0300

    standard cfs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ let quotes = [
     "Sometimes we will never know the true value of a moment untill it becomes a memory",
     "Follow your curiosity",
     "A goal without a plan is just a wish",
-    "Stay humble, Good times will coome"
+    "Never talk about your riches in the midst if the poor",
+    "Get paid. Not played",
+    "Stay humble, Good times will come"
+    // more quotes to be added
 ]
 const noRpeat = new Set();
 const showQuote = document.getElementById("getQuote");
@@ -10,8 +13,13 @@ const generate = document.getElementById("submitBtn");
 
 
 function generateQuote() {
+    while(true) {
     let randomQuote = Math.floor(Math.random() * quotes.length);
+
+    if(noRpeat.has) continue
+
     let quote = quotes[randomQuote]
     showQuote.innerHTML = quote;
+    }
 }
 generate.onclick = generateQuote;
