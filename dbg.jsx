78d723a9916374244393c76567493a1ddfcd87df
nianod <arnoldkk422@gmail.com>
2025-03-24 21:16:19 +0300

    final commit

---
+++ dbg.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import store from "./app/store";
+import { Provider } from "react-redux";
+import { injectStore } from "./utils/axiosCreate";
+import App from "./App";
+
+injectStore(store);
+
+const getQuote = ReactDOM.createRoot(document.getElementById("getQuote"));
+getQuote.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
