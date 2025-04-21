b00093dd28f23ea1be5eaa9774162f570ae2c6be
nianod <arnoldkk422@gmail.com>
2025-04-21 21:09:19 +0300

    final

--- dbg.jsx
+++ dbg.jsx
@@ -3,18 +3,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import store from "./app/store";
-import { Provider } from "react-redux";
-import { injectStore } from "./utils/axiosCreate";
-import App from "./App";
+// import store from "./app/store";
+// import { Provider } from "react-redux";
+// import { injectStore } from "./utils/axiosCreate";
+// import App from "./App";
 
-injectStore(store);
+// injectStore(store);
 
-const getQuote = ReactDOM.createRoot(document.getElementById("getQuote"));
-getQuote.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+// const getQuote = ReactDOM.createRoot(document.getElementById("getQuote"));
+// getQuote.render(
+//   <React.StrictMode>
+//     <Provider store={store}>   
+//       <App />
+//     </Provider>
+//   </React.StrictMode>
+// );
